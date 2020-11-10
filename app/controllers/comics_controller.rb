ae19e0fba8b6e4baa6811e244d2136a0aa1eea9e
DSJR1284 <DSJR1284@GMAIL.COM>
2020-11-10 18:02:52 -0500

    working on comic routes and views

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,30 +1,35 @@
 class ComicsController < ApplicationController
 
-  # GET: /comics
+ 
   get "/comics" do
     @comics = current_user.comics
       erb :"comics/index"
-  end
+  end 
 
-    # GET: /comics/5
-    get "/comics/:id" do      
+    # GET: /comics/new
+    get "/comics/new" do
+      erb :"/comics/new"
      end
+   
+     post "/comics" do 
+        comic = Comic.create(title: params[:title], image_url: params[:image_url], blurb: params[:blurb], user_id: current_user.id)
+        redirect "/comics/#{comic.id}"
+     end 
 
-  # GET: /comics/new
-  get "/comics/new" do
-    erb :"/comics/new"
-  end
+       # GET: /comics/5    
+      get "/comics/:id" do 
+        @comics = Comic.find(params[:id])
+        erb :"/comics/show"
+      end
+       # POST: /comics
+      post "/comics" do
 
-  # POST: /comics
-  post "/comics" do
-    redirect "/comics"
-  end
-
-
+   
+      end
 
   # GET: /comics/5/edit
   get "/comics/:id/edit" do
-    erb :"/comics/edit"
+ 
   end
 
   # PATCH: /comics/5
@@ -34,7 +39,7 @@ class ComicsController < ApplicationController
 
   # DELETE: /comics/5/delete
   delete "/comics/:id/delete" do
-    redirect "/comics"
+  
   end
 
 end
